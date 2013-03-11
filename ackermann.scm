aac4cde0cd5ffb8d8dd5b21c73775e8c42282c02
pld <peter@helioid.com>
2013-03-11 00:41:40 -0400

    add tetration operator

--- ackermann.scm
+++ ackermann.scm
@@ -1,3 +1,7 @@
+;; Ackermann function, e.g.
+;;   multiplication, (A 0 n) => 2n
+;;   exponentiation, (A 1 n) => 2^{n}
+;;   tetration, (A 2 n) => ^{n}2
 (define (A x y)
  (cond ((= y 0) 0)
        ((= x 0) (* 2 y))
@@ -5,6 +9,8 @@
        (else (A (- x 1)
                 (A x (- y 1))))))
 
-;; multiplication, (A 0 n) => 2n
-;; exponentiation, (A 1 n) => 2^{n}
-;; tetration, (A 2 n) => ^{n}2
+;; Tetration operator
+(define (^^ a n)
+ (if (= n 0)
+     1
+     (expt a (^^ a (- n 1)))))
